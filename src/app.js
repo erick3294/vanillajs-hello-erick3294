e8177a08cf3a7b6b2df8faca433cfa3b673d19c8
Erick Sanchez <erick94310@gmail.com>
2025-07-18 20:08:18 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+ let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let url = []
+  pronoun.forEach(
+    (words)=> {
+    url.push(words)
+     
+    }
+  )
+  let firstWord = url[0]
+  let secondWord = url[1]
+  let finalUrl = firstWord + secondWord
+  console.log(finalUrl)
+
   console.log("Hello Rigo from the console!");
 };
